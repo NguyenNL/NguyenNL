d684ab4b4e779377028023fc015741adae79c5c5
NguyenNL <lenguyen051094@gmail.com>
2017-12-28 06:41:01 +0700

    add first

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about-container">
+    <h1>About Page</h1>
+    <p>Hello, I am Nguyen :)</p>
+    <p>Font-end developer at FPT software</p>
+  </div>
+</template>
+<style>
+  .about-container {
+
+  }
+</style>
